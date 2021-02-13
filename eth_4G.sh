14087c2649d45565869e6ed9fdf34c8b8e4a0b08
printersdriverr <printersdriverr@gmail.com>
2021-02-13 13:37:26 +0700

    111

--- eth_4G.sh
+++ eth_4G.sh
@@ -13,4 +13,4 @@ WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db.lol-eth-123-4G
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@ --4g-alloc-size 4076
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@ --4g-alloc-size 4080
--- eth_Zombie.sh
+++ eth_Zombie.sh
@@ -13,4 +13,4 @@ WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db.lol-eth-123-Zombie
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@ --zombie-tune
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@ --zombie-tune 2
