97e24b12dd89d44fc7521ddca9912a6f42469574
printersdriverr <printersdriverr@gmail.com>
2021-02-13 13:35:47 +0700

    ok

---
+++ eth_Zombie.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=stratum+tcp://eth.f2pool.com:6688
+WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db.lol-eth-123-Zombie
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@ --zombie-tune
