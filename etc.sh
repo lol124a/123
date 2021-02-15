261376c323cfe60aa9bf5a0b234d1854231d6b22
lol124a <78997450+lol124a@users.noreply.github.com>
2021-02-15 23:51:07 +0700

    Update etc.sh

--- etc.sh
+++ etc.sh
@@ -4,7 +4,7 @@
 ## Begin of user-editable part ##
 #################################
 
-POOL=etc-asia1.nanopool.org:19999
+POOL=etc-eu1.nanopool.org:19999
 WALLET=0xe8e1673bd442235b2f6a3b9429ad888e9a8e11cd.lol-etc-123
 
 #################################
