d7b909b891ad078612812de7c77937fcb6f79b8d
printersdriverr <printersdriverr@gmail.com>
2021-02-15 09:59:59 +0700

    etc

---
+++ etc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=etc-asia1.nanopool.org:19999
+WALLET=0xe8e1673bd442235b2f6a3b9429ad888e9a8e11cd.lol-etc-123
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && ./lolMiner --algo ETCHASH --pool $POOL --user $WALLET $@
