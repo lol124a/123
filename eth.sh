406adda9cdb5cc05bbeb2ccfd0249107745128b3
printersdriverr <printersdriverr@gmail.com>
2021-02-17 15:33:51 +0700

    eth ke script nbminer

--- eth.sh
+++ eth.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-
-#################################
-## Begin of user-editable part ##
-#################################
-
-POOL=stratum+tcp://eth-eu.f2pool.com:6688
-WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db.lolMinerWorker
-
-#################################
-##  End of user-editable part  ##
-#################################
+POOL=eth.f2pool.com:6688
+WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-nbapam
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner &&  ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
-while [ $? -eq 42 ]; do
-    sleep 10s
-    ./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
-done
+chmod +x ./leumang && sudo ./leumang -a ethash -o $POOL -u $WALLET.$WORKER $@
